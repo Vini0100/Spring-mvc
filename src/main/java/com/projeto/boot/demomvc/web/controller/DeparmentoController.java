61b58f91856930d1d350b7a07e9f29fcf67c849e
Vinícius Andrade Gonzato <126361791+Vini0100@users.noreply.github.com>
2024-03-11 22:05:02 -0300

    Add Controller e View - departamentos

--- src/main/java/com/projeto/boot/demomvc/web/controller/DeparmentoController.java
+++ src/main/java/com/projeto/boot/demomvc/web/controller/DeparmentoController.java
@@ -1,20 +1,35 @@
 package com.projeto.boot.demomvc.web.controller;
 
+import com.projeto.boot.demomvc.domain.Departamento;
+import com.projeto.boot.demomvc.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DeparmentoController {
 
+    @Autowired
+    private DepartamentoService service;
+
     @GetMapping("/cadastrar")
-    public String cadastrar() {
+    public String cadastrar(Departamento departamento) {
      return "/departamento/cadastro";
     }
     @GetMapping("/listar")
-    public String listar() {
+    public String listar(ModelMap model) {
+        model.addAttribute("departamentos", service.buscarTodos());
         return "/departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento){
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 }
 
